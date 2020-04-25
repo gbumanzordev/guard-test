c584904031a5cf61913e87f7c0a212b9297dd7cd
Gerson <gbumanzor@outlook.com>
2020-04-25 15:35:19 -0600

    :sparkles: Create component for first route.

---
+++ src/app/components/first-route/first-route.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-first-route',
+  templateUrl: './first-route.component.html',
+  styleUrls: ['./first-route.component.css']
+})
+export class FirstRouteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
